b8289b81fe4102b74c94fdb018c714df5f2fb9fa
david sanchez <david-sanchez@maxcorp.com.co>
2018-03-12 17:13:14 -0500

    Update README

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -10,14 +10,9 @@
  */
 
 import React from 'react';
-import Board from 'components/Board';
 import {
   Container,
-  Grid,
   Header,
-  Button,
-  Icon,
-  Label,
 } from 'semantic-ui-react';
 
 export default class HomePage extends React.PureComponent {
@@ -26,43 +21,7 @@ export default class HomePage extends React.PureComponent {
   render() {
     return (
       <Container style={{ marginTop: '10px' }}>
-        <Grid textAlign="center" columns={2} doubling>
-          <Grid.Row>
-            <Header size="huge">Reversi Game</Header>
-          </Grid.Row>
-          <Grid.Row>
-            <Board />
-          </Grid.Row>
-          <Grid.Row>
-            <Grid.Column>
-              <Button as="div" labelPosition="left">
-                <Label basic pointing="right">
-                  64
-                </Label>
-                <Button icon basic>
-                  White
-                  <Icon inverted color="grey" name="circle" />
-                </Button>
-              </Button>
-            </Grid.Column>
-
-            <Grid.Column>
-              <Button as="div" labelPosition="right">
-                <Button icon basic>
-                  <Icon name="circle" />
-                  Black
-                </Button>
-                <Label basic pointing="left">
-                  64
-                </Label>
-              </Button>
-            </Grid.Column>
-          </Grid.Row>
-
-          <Grid.Row>
-            <Button basic color="blue" content="Restart" icon="refresh" />
-          </Grid.Row>
-        </Grid>
+        <Header size="huge">Prueba Tecnica</Header>
       </Container>
     );
   }
